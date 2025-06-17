a652b7c903a1f9b1bc665ee2e97d2f67946bc492
charlieroberts <charlie@charlie-roberts.com>
2025-06-17 09:08:51 -0700

    add property to force tail allocation and method to get next tail block

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 let MemoryHelper = {
+  
   create( sizeOrBuffer=4096, memtype=Float32Array ) {
     let helper = Object.create( this )
 
@@ -9,7 +10,12 @@ let MemoryHelper = {
     Object.assign( helper, {
       heap: new memtype( sizeOrBuffer ),
       list: {},
-      freeList: {}
+      freeList: {},
+
+      // if useTail is true, will force MemoryHelper to allocate at end of the
+      // heap and skip any freed memory blocks. Useful to force a contiguous
+      // block of memory
+      useTail:false,
     })
 
     return helper
@@ -22,27 +28,29 @@ let MemoryHelper = {
       throw Error( 'Allocation request is larger than heap size of ' + this.heap.length )
     }
 
-    for( let key in this.freeList ) {
-      let candidate = this.freeList[ key ]
+    if( this.useTail === false ) {
+      for( let key in this.freeList ) {
+        let candidate = this.freeList[ key ]
 
-      if( candidate.size >= size ) {
-        idx = key
+        if( candidate.size >= size ) {
+          idx = key
 
-        this.list[ idx ] = { size, immutable, references:1 }
+          this.list[ idx ] = { size, immutable, references:1 }
 
-        if( candidate.size !== size ) {
-          let newIndex = idx + size,
-              newFreeSize
+          if( candidate.size !== size ) {
+            let newIndex = idx + size,
+                newFreeSize
 
-          for( let key in this.list ) {
-            if( key > newIndex ) {
-              newFreeSize = key - newIndex
-              this.freeList[ newIndex ] = newFreeSize
+            for( let key in this.list ) {
+              if( key > newIndex ) {
+                newFreeSize = key - newIndex
+                this.freeList[ newIndex ] = newFreeSize
+              }
             }
           }
-        }
 
-        break
+          break
+        }
       }
     }
 
@@ -66,12 +74,31 @@ let MemoryHelper = {
     if( idx + size >= this.heap.length ) {
       throw Error( 'No available blocks remain sufficient for allocation request.' )
     }
+
+    return idx
+  },
+
+  // this returns the next index that will be use by 
+  // memory helper, unless there are freed blcoks available.  
+  // if the useTail property is set to true this will return
+  // the next block index regardless of any freed blocks.
+  getLastUsedIndex() {
+    let keys = Object.keys( this.list ),
+        idx = 0,
+        lastIndex
+
+    if( keys.length ) { // if not first allocation...
+      lastIndex = parseInt( keys[ keys.length - 1 ] )
+
+      idx = lastIndex + this.list[ lastIndex ].size
+    }
+
     return idx
   },
 
   addReference( index ) {
     if( this.list[ index ] !== undefined ) { 
-      this.list[ index ].references++
+      this.list[ index ].references++https://x.com/homegit
     }
   },
 
